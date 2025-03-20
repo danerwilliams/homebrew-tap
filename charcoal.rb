dea759c403053ede5581e0e9d014833d16cc403a
Dane Williams <dane_williams@outlook.com>
2025-03-19 21:10:12 -0700

    Release Charcoal `v0.2.4` (#18)

--- charcoal.rb
+++ charcoal.rb
@@ -1,21 +1,21 @@
 class Charcoal < Formula
   desc "Charcoal allows you to easily manage your stacked-diff workflow."
   homepage "https://github.com/danerwilliams/charcoal"
-  version "0.2.3"
+  version "0.2.4"
   license "apgl-3.0"
 
   depends_on "gh" => ">= 2.0.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.3/gt-macos-arm64"
-      sha256 "82051b0587a9b5dfa0175e8d9d042dd690a301ef5408b379eb249356523cba0f"
+      url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.4/gt-macos-arm64"
+      sha256 "b0bff69a3807446849ec2630c0dc0f098cc2b93623ea546ef8c04ab6d88f6067"
       def install
         bin.install "gt-macos-arm64" => "gt"
       end
     else
-      url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.3/gt-macos-x64"
-      sha256 "76a9981e7882ae15495daf1178b0263860b9a1b2cd95c795aaf16cd6b5a98ffa"
+      url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.4/gt-macos-x64"
+      sha256 "45adbf0e6d13380545a63c11b91f60b7d52129504116d757bc310a9560a5e08d"
       def install
         bin.install "gt-macos-x64" => "gt"
       end
@@ -23,8 +23,8 @@ class Charcoal < Formula
   end
 
   if OS.linux?
-    url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.3/gt-linux"
-    sha256 "a16d1e17472e94faa00326b89b6a610a256ee9e8de0a772d3967938603d5348b"
+    url "https://github.com/danerwilliams/charcoal/releases/download/v0.2.4/gt-linux"
+    sha256 "686bd9e3ad2ea7584ac4942c553387e238e1b9ebd25bbb4ededb00f6cdc60f96"
     def install
       bin.install "gt-linux" => "gt"
     end  
